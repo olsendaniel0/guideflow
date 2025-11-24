ad32409132871e994d04f799aab96becfef3b6b3
AFS0019627 <AFS0019627@agderskolen.no>
2025-11-24 22:59:19 +0000

    Oppdatering

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: 50 }}>
+      <h1>Velkommen til GuideFlow!</h1>
+      <p>Her er et testbilde av Daniel:</p>
+      <Image src="/Daniel.jpg" alt="Testbilde" width={500} height={300} />
+    </div>
+  );
+}
