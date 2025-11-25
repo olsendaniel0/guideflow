8f3c56c7a65f861a2e0b90208a50af56db2b6fc1
AFS0019627 <AFS0019627@agderskolen.no>
2025-11-25 19:10:28 +0000

    temp name

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,13 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div style={{ textAlign: "center", marginTop: 50 }}>
-      <h1>Velkommen til GuideFlow!</h1>
-      <p>Her er et testbilde av Daniel:</p>
-      <Image src="/daniel.JPG" alt="Testbilde" width={500} height={300} />
+    <div style={{textAlign: "center"}}>
+      <h1 style={{color: "red", fontSize: 36,
+         textAlign: "center",mariginTop: 20}}
+          >Velkommen til GuideFlow!</h1>
+      <p style={{color: "purple", fontSize: 24,
+         textAlign: "center",mariginTop: 20}}>Her er et testbilde av Daniel:</p>
+      <Image src="/daniel.JPG" alt="Testbilde" width={1000} height={600}/>
     </div>
   );
 }
